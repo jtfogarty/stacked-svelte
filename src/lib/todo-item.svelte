59a4f079dd0ab4fa65e5a43cb7241bdf916e5298
Jeff Fogarty <jtf.github@gmail.com>
2022-03-07 02:19:53 +0000

    starting html structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="">
+        <input  type="hidden" name="done" value="" />
+        <button aria-label="Mark done/not done" >Done/Not Done</button>
+    </form>
+
+    <form action="" method="">
+        <input  type="text" />
+        <button aria-label="Save ToDo" >Save</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="Delete ToDo" >Delete</button>
+    </form>
+</div>
